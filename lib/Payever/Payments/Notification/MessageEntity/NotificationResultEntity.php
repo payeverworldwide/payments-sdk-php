fd2378a166393456e3028ec5f3a07ea419174d03
payever <service@payever.de>
2023-09-04 06:44:04 +0000

    Version 1.2.4 autorelease

--- lib/Payever/Payments/Notification/MessageEntity/NotificationResultEntity.php
+++ lib/Payever/Payments/Notification/MessageEntity/NotificationResultEntity.php
@@ -21,12 +21,20 @@ use Payever\Sdk\Payments\Http\MessageEntity\RetrievePaymentResultEntity;
  *
  * @method float                getRefundAmount()
  * @method float                getCaptureAmount()
+ * @method float                getCancelAmount()
+ * @method float                getTotalCapturedAmount()
+ * @method float                getTotalRefundedAmount()
+ * @method float                getTotalCanceledAmount()
  * @method array                getRefundedItems()
  * @method array                getCapturedItems()
  * @method self                 setRefundAmount()
  * @method self                 setCaptureAmount()
+ * @method self                 setCancelAmount()
  * @method self                 setRefundedItems()
  * @method self                 setCapturedItems()
+ * @method self                 setTotalCapturedAmount()
+ * @method self                 setTotalRefundedAmount()
+ * @method self                 setTotalCanceledAmount()
  *
  * @SuppressWarnings(PHPMD.ShortVariable)
  * @SuppressWarnings(PHPMD.TooManyFields)
@@ -35,15 +43,21 @@ class NotificationResultEntity extends RetrievePaymentResultEntity
 {
     /**
      * Returns the total (partial) refunded amount for this transaction
-     * @var float
+     * @var float|null
      */
     protected $refundAmount;
 
     /**
      * Returns the total (partial) captured amount for this transaction
-     * @var float
+     * @var float|null
      */
     protected $captureAmount;
+
+    /**
+     * Returns the total (partial) cancelled amount for this transaction
+     * @var float|null
+     */
+    protected $cancelAmount;
 
     /**
      * Refunded items (only when cart was provided)
@@ -56,4 +70,22 @@ class NotificationResultEntity extends RetrievePaymentResultEntity
      * @var array
      */
     protected $capturedItems;
+
+    /**
+     * Returns the total captured amount for payment
+     * @var float|null
+     */
+    protected $totalCapturedAmount;
+
+    /**
+     * Returns the total refunded amount for payment
+     * @var float|null
+     */
+    protected $totalRefundedAmount;
+
+    /**
+     * Returns the total cancelled amount for payment
+     * @var float|null
+     */
+    protected $totalCanceledAmount;
 }
